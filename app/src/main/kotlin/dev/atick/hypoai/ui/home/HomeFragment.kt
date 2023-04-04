d81e63b4cf9c8eaf05f16f21bcf98f69846eeef2
Atick Faisal <atickfaisal@gmail.com>
2023-04-05 01:43:22 +0300

    Improve UI

--- app/src/main/kotlin/dev/atick/hypoai/ui/home/HomeFragment.kt
+++ app/src/main/kotlin/dev/atick/hypoai/ui/home/HomeFragment.kt
@@ -1,14 +1,22 @@
 package dev.atick.hypoai.ui.home
 
+import android.content.Intent
+import android.net.Uri
 import androidx.compose.runtime.Composable
 import dagger.hilt.android.AndroidEntryPoint
 import dev.atick.core.ui.base.BaseFragment
+
 
 @AndroidEntryPoint
 class HomeFragment : BaseFragment() {
 
     @Composable
     override fun ComposeUi() {
-        HomeScreen()
+        HomeScreen(::openHypoAiWebsite)
+    }
+
+    private fun openHypoAiWebsite() {
+        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://hypospadias-ai.netlify.app/"))
+        startActivity(browserIntent)
     }
 }
